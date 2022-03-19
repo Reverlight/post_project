688467c282cddf8debd702c62c4b01e2b320b45a
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-03-19 14:10:54 +0200

    User login view

--- post_app/urls.py
+++ post_app/urls.py
@@ -5,14 +5,14 @@ from .views import (
     creation,
     like,
     analytics,
-    login,
+    LoginAPIView,
     main
 )
 
 urlpatterns = [
     path('', main, name='main'),
     path('user/signup/', SignupAPIView.as_view(), name='signup'),
-    path('user/login/', login, name='login'),
+    path('user/login/', LoginAPIView.as_view(), name='login'),
     path('post/creation/', creation, name='creation'),
     path('post/like/', like, name='like'),
     path('analytics/', analytics, name='analytics')

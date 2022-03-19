38262b28b6f4df5e208c40a2068ddea00c9744af
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-03-19 13:24:55 +0200

    User login serializer

--- post_app/serializers.py
+++ post_app/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from rest_framework import serializers
 
 from post_app.models import User
@@ -18,3 +19,46 @@ class UserSignupSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    email = serializers.CharField(max_length=255)
+    username = serializers.CharField(max_length=255, read_only=True)
+    password = serializers.CharField(max_length=128, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password', 'token']
+
+    def validate(self, data):
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        if email is None:
+            raise serializers.ValidationError(
+                'Email address is required to log in'
+            )
+
+        if password is None:
+            raise serializers.ValidationError(
+                'Password is required'
+            )
+
+        user = authenticate(username=email, password=password)
+
+        if user is None:
+            raise serializers.ValidationError(
+                'There is no such user with specified email and password pair'
+            )
+
+        if not user.is_active:
+            raise serializers.ValidationError(
+                'User has been deactivated '
+            )
+
+        return {
+            'email': user.email,
+            'username': user.username,
+            'token': user.token
+        }

4ca4f6a69a4c4e0499bf02fd8a4d7a5e25ffd117
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-03-19 10:59:13 +0200

    Signup view

--- post_app/views.py
+++ post_app/views.py
@@ -1,14 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from .serializers import UserSignupSerializer
 
 def main(request):
     return HttpResponse('<h1>Future Main</h1>')
 
 
-def signup(request):
-    return HttpResponse('<h1>Future Sign up</h1>')
+class SignupAPIView(APIView):
+    permission_classes = (AllowAny, )
+    serializer_class = UserSignupSerializer
 
+    def post(self, request):
+        user = request.data.get('user', {})
+
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 def login(request):
     return HttpResponse('<h1>Future Login</h1>')

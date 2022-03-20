71099bc0c2f696f7aedd58ffcdbd9f112ea5af85
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-03-20 11:17:11 +0200

    User login API
    * User login serializer
    
    * User login view
    
    * User serializer
    
    * User view
    
    * Services for decode/encode token

--- post_app/urls.py
+++ post_app/urls.py
@@ -2,17 +2,19 @@ from django.urls import path
 
 from .views import (
     SignupAPIView,
+    UserAPIView,
     creation,
     like,
     analytics,
-    login,
+    LoginAPIView,
     main
 )
 
 urlpatterns = [
     path('', main, name='main'),
     path('user/signup/', SignupAPIView.as_view(), name='signup'),
-    path('user/login/', login, name='login'),
+    path('user/', UserAPIView.as_view(), name='user-data'),
+    path('user/login/', LoginAPIView.as_view(), name='login'),
     path('post/creation/', creation, name='creation'),
     path('post/like/', like, name='like'),
     path('analytics/', analytics, name='analytics')
--- post_app/views.py
+++ post_app/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 from rest_framework import status
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import UserSignupSerializer
+from .models import User
+from .renderers import UserJSONRenderer
+from .serializers import UserSignupSerializer, UserLoginSerializer, UserSerializer
+from .services import decode_token
+
 
 def main(request):
     return HttpResponse('<h1>Future Main</h1>')
@@ -24,8 +28,40 @@ class SignupAPIView(APIView):
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-def login(request):
-    return HttpResponse('<h1>Future Login</h1>')
+
+class LoginAPIView(APIView):
+    permission_classes = (AllowAny, )
+    renderer_classes = (UserJSONRenderer, )
+    serializer_class = UserLoginSerializer
+
+    def post(self, request):
+        user = request.data.get('user', {})
+
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer_data = serializer.data
+
+        response = Response()
+        response.set_cookie(key='token', value=serializer_data['token'])
+        response.data = serializer_data
+        response.status_code = 200
+
+        return response
+
+
+class UserAPIView(APIView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserLoginSerializer
+    renderer_classes = (UserJSONRenderer,)
+
+    def get(self, request):
+        token = request.COOKIES.get('token')
+        payload = decode_token(token)
+
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = self.serializer_class(user)
+
+        return Response(serializer.data)
 
 
 def analytics(request):

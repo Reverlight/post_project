c169ccf596ba39ca0536aa072050e29cc67d8722
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-03-20 11:09:11 +0200

    Services for decode/encode token

---
+++ post_app/services.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+
+import jwt
+from django.conf import settings
+from rest_framework.exceptions import AuthenticationFailed
+
+
+def encode_token(pk):
+    dt = datetime.now() + timedelta(days=1)
+
+    return jwt.encode(
+        {
+            'id': pk,
+            'exp': int(dt.strftime('%m%d%Y%H%M%S'))
+        },
+        settings.SECRET_KEY,
+        algorithm='HS256'
+    )
+
+
+def decode_token(token):
+    if not token:
+        raise AuthenticationFailed('Unauthenticated!')
+    try:
+        return jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms='HS256'
+        )
+    except jwt.ExpiredSignatureError:
+        raise AuthenticationFailed('Token is expired')
+    except jwt.exceptions.DecodeError:
+        raise AuthenticationFailed('Token is incorrect')

3bb62d7402094ab3c2dbd7ed74d6465d2f3f081c
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-03-21 16:21:35 +0200

    Sign up page
    * User Model Form
    
    * Sign up page view
    
    * Sign up page html template

---
+++ post_app/static/js/user_signup.js
@@ -0,0 +1,41 @@
+window.addEventListener('load', () => {
+    const username = document.querySelector('input[name="username"]')
+    const email = document.querySelector('input[name="email"]')
+    const password = document.querySelector('input[name="email"]')
+    const csrfToken = document.querySelector('input[name="csrfmiddlewaretoken"]')
+    const signupUrl = window.location + 'api/'
+    const message = document.querySelector('#message')
+    const form = document.querySelector('form')
+
+    async function sendSignupRequest(data) {
+        await fetch(signupUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrfToken.value
+            },
+            body: JSON.stringify(data)
+        }).then(response => {
+            response.text().then((errorText) => {
+                    if (response.status === 400){
+                        message.innerHTML = errorText
+                    }
+                    else if (response.status === 201){
+                        message.innerHTML = 'Success!!!'
+                    }
+                })
+        })
+    }
+
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault()
+        const data = {
+            'user': {
+                'username': username.value,
+                'email': email.value,
+                'password': password.value
+            }
+        }
+        await sendSignupRequest(data)
+    })
+});

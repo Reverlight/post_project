cdcf6ec7d278ddcfdd00b820ea057efab9a17fd8
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-03-21 14:37:48 +0200

    User Form

--- post_app/forms.py
+++ post_app/forms.py
@@ -1,9 +1,15 @@
 from django.forms import ModelForm
 
-from .models import Post
+from .models import Post, User
 
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'text']
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']

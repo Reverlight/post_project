84a02e1d570a91ea7ad21dc65911b871d08b6127
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-03-22 19:56:19 +0200

    Middleware for update_user

--- core/settings.py
+++ core/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'post_app.middleware.UpdateUserMiddleware'
 ]
 
 ROOT_URLCONF = 'core.urls'

f2df68a3012dafdaad1eaa5d70d432f7a1efa96f
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-03-22 19:56:40 +0200

    Deleted decorator

--- post_app/decorators.py
+++ post_app/decorators.py
@@ -1,17 +1,4 @@
-from .models import User
 from .services import decode_token
-
-
-def update_last_request(func):
-    def wrapper(obj, request):
-        user = request.data.get('user', {})
-        email = user['email']
-
-        user = User.objects.filter(email=email).first()
-        user.update_last_request()
-        return func(obj, request)
-
-    return wrapper
 
 
 def login_required(func):

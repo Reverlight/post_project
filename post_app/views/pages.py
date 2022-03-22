59890b811541394e545f060c71dd2944d6847d94
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-03-22 18:49:57 +0200

    Like analytics

--- post_app/views/pages.py
+++ post_app/views/pages.py
@@ -2,10 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from ..forms import UserForm, UserLoginForm
+from ..models import Like
+from ..services import parse_date
 
 
 def analytics(request):
-    return HttpResponse('<h1>Future Analytics</h1>')
+    _from = request.GET.get('date_from')
+    _to = request.GET.get('date_to')
+
+    if not _from or not _to:
+        return HttpResponse(f'<h1>Please specify params date_from, date_to</h1>')
+
+    date_from = parse_date(request.GET.get('date_from'))
+    date_to = parse_date(request.GET.get('date_to'))
+
+    r = Like.objects.filter(made_at_time__range=(date_from, date_to)).count()
+    return HttpResponse(f'<h1>Future Analytics</h1> {r}')
 
 
 def main(request):

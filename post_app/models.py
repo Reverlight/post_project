f3f95b7d925e7059d45ebc2589bd4c1054449c39
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-03-22 12:22:46 +0200

    def __str__ for Post

--- post_app/models.py
+++ post_app/models.py
@@ -15,6 +15,9 @@ class Post(models.Model):
     text = models.TextField()
     created_by = models.ForeignKey('User', on_delete=models.CASCADE, db_index=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Like(models.Model):
     user = models.ForeignKey('User', on_delete=models.CASCADE, db_index=True)

96136000ddfa93f372ee120b21b34ac6b75678f8
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-03-30 12:06:16 +0300

    Example urls in comments

--- post_app/urls.py
+++ post_app/urls.py
@@ -16,6 +16,23 @@ from .views.pages import (
     main
 )
 
+'''
+urlpatterns = [
+    path('', main, name='main'),
+    path('user/signup/', signup, name='signup'),
+    path('user/login/', login, name='login'),
+    path('user/', UserAPIView.as_view(), name='user'),
+    path('post/creation/', PostCreate.as_view(), name='creation'),
+    path('post/<int:pk>/', PostDetail.as_view(), name='detail'),
+    path('post/list/', PostList.as_view(), name='post-list'),
+
+    path('api/v1/user/signup/', SignupAPIView.as_view(), name='signup-api'),
+    path('api/v1/user/login/', LoginAPIView.as_view(), name='login-api'),
+    path('api/v1/analytics/', analytics_api, name='analytics-api'),
+    path('api/v1/post/<int:pk>/like/', like_api, name='like-api'),
+]
+'''
+
 urlpatterns = [
     path('', main, name='main'),
     path('user/signup/api/', SignupAPIView.as_view(), name='signup-api'),

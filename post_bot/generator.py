835da1174dde0b0d666a51c9346176aeada08864
Reverlight <artem3766@gmail.com>
2023-03-06 12:00:09 +0200

    Add type_path mapping to generator

--- post_bot/generator.py
+++ post_bot/generator.py
@@ -1,18 +1,27 @@
 import random
+from datetime import datetime
+from post_bot.config import SENTENCES_PER_POST, PASSWORD_LENGTH, PASSWORD_CHARACTERS, EMAIL_DOMAINS, RANDOM_NAMES_PATH, RANDOM_SENTENCES_PATH
 
-from post_bot.config import SENTENCES_PER_POST, PASSWORD_LENGTH, PASSWORD_CHARACTERS, EMAIL_DOMAINS
+TYPE_PATH_MAPPING = {
+    'names': RANDOM_NAMES_PATH,
+    'sentences': RANDOM_SENTENCES_PATH,
+}
 
 
-def read_lines_from_file(file_path):
+def get_data(type):
+    return _read_lines_from_file(TYPE_PATH_MAPPING[type])
+
+
+def _read_lines_from_file(file_path):
     result = []
     with open(file_path, 'r', encoding='UTF-8') as file:
         for line in file:
-            result.append(line)
+            result.append(line.replace('\n', '').replace(' ',  '-'))
     return result
 
 
 def _generate_username(users):
-    return f'bot-user-{random.choice(users)}'
+    return f'bot-user-{random.choice(users)}-{datetime.utcnow().second}'
 
 
 def _generate_password():
